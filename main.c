34926ba5bf14f17c22721fa8c5fe76e447b534fa
Flexie <burlutskiyts@gmail.com>
2024-11-28 01:50:12 +0300

    split(ed)

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <string.h>
-
-#define SYMBOLS " \t"
-#define PROMPT "Вводите строчку: "
-
-char *remove_spaces(const char *str, int *count);
-int sort(const char *new_str, int count);
+#include "head.h"
 
 int main()
 {
@@ -26,99 +20,6 @@ int main()
 	free(new_str);
 	str = readline(PROMPT);
     }
-    return 0;
-}
-
-char *remove_spaces(const char *str, int *count)
-{
-    printf("Ваша строчка: \"%s\"\n", str);
-    int len = 0;
-    int word_count = 0;
-    int str_len = strlen(str);
-    char *res = calloc(str_len + 1, sizeof(char));
-    if (res == NULL) {
-	printf("Ошибка выделения памяти\n");
-	return NULL;
-    }
-    char *s = strdup(str);
-    if (s == NULL) {
-	printf("Ошибка выделения памяти\n");
-	return NULL;
-    }
-    char *word = strtok(s, SYMBOLS);
-    while (word != NULL) {
-	int word_len = strlen(word);
-	strncpy(res + len, word, word_len);
-	len += word_len;
-	res[len] = ' ';
-	len++;
-	word = strtok(NULL, SYMBOLS);
-	word_count++;
-    }
-    free(s);
-    if (len > 0) {
-	len--;
-    }
-    res[len]='\0';
-    char *b = realloc(res, (len+1)*sizeof(char));
-    if (b == NULL) {
-	printf("Ошибка выделения памяти");
-	return NULL;
-    }
-    res = b;
-    printf("Получите адекватную строчку: \"%s\"\n", res);
-    *count = word_count;
-    return res;
-}
-
-int sort(const char *new_str, int count)
-{
-    char *s = strdup(new_str);
-    int *len = malloc(sizeof(int) * count);
-    char **words = malloc(sizeof(char *) * count);
-    if (s == NULL || len == NULL || *words == NULL) {
-        printf("Ошибка выделения памяти\n");
-        return 1;
-    }
-    char *word = strtok(s, SYMBOLS);
-    for (int i = 0; i < count && word != NULL; i++) {
-	words[i] = strdup(word);
-	if (words[i] == NULL) {
-	    printf("Ошибка выделения памяти\n");
-	    return 1;
-	}
-	len[i] = strlen(word);
-	word = strtok(NULL, SYMBOLS);
-    }
-    int index;
-    for (int i = 0; i < count - 1; i++) {
-	index = i;
-	for (int j = i + 1; j < count; j++) {
-	    if (len[index] > len[j]) {
-		index = j;
-	    }
-	}
-	if (index != i) {
-	    int temp_len = len[i];
-	    len[i] = len[index];
-	    len[index] = temp_len;
-
-	    char *temp_word = words[i];
-	    words[i] = words[index];
-	    words[index] = temp_word;
-	}
-    }
-    printf("Отсортированная по возрастанию: \"");
-    for (int i = 0; i < count; i++) {
-            printf("%s ", words[i]);
-    }
-    printf("\"\n");
-    for (int i = 0; i < count; i++) {
-        free(words[i]);
-    }
-    free(s);
-    free(len);
-    free(words);
     return 0;
 }
 
