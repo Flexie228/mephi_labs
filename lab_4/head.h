a5e070462a4619e802bae1f4a556e7019ab3aa7c
Flexie <burlutskiyts@gmail.com>
2024-11-29 15:30:09 +0300

    add lab_4

---
+++ lab_4/head.h
@@ -0,0 +1,9 @@
+#ifndef KAF_12
+#define KAF_12
+
+#define SYMBOLS " \t"
+#define PROMPT "Вводите строчку: "
+char *remove_spaces(const char *str, int *count);
+int sort(const char *new_str, int count);
+
+#endif
