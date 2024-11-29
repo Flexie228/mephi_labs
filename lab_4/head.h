1db91823ebfeba1abebb589628b5367119b6cf2c
Flexie <burlutskiyts@gmail.com>
2024-11-29 16:06:25 +0300

    delete lab_4

--- lab_4/head.h
+++
@@ -1,9 +0,0 @@
-#ifndef KAF_12
-#define KAF_12
-
-#define SYMBOLS " \t"
-#define PROMPT "Вводите строчку: "
-char *remove_spaces(const char *str, int *count);
-int sort(const char *new_str, int count);
-
-#endif
